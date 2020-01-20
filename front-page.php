3715c8189ba99446b4dbe67a1d85bd3312b8a3e3
Akevia <akevia@Akevias-iMac.local>
2020-01-20 17:04:55 -0600

    cambios en home

--- front-page.php
+++ front-page.php
@@ -195,34 +195,43 @@
         <div class="column">
             <div class="form">
                 <div class="title">Contáctanos</div>
-                <div class="formrow">
-                    <span><img src="" alt=""></span>
-                    <input type="text" placeholder="Nombre">
+                <div class="form-row">
+                    <div class="form-input">
+                        <div class="icon person"></div>
+                        <input type="text" placeholder="Nombre">
+                    </div>
+                    <div class="form-input">
+                        <div class="icon mail"></div>
+                        <input type="text" placeholder="Correo">
+                    </div>
                 </div>
-                <div class="formrow">
-                    <span><img src="" alt=""></span>
-                    <input type="text" placeholder="Correo">
+                <div class="form-row">
+                    <div class="form-input">
+                        <div class="icon phone"></div>
+                        <input type="text" placeholder="Teléfono">
+                    </div>
                 </div>
-                <div class="formrow">
-                    <span><img src="" alt=""></span>
-                    <input type="text" placeholder="Teléfono">
+
+                <div class="form-row">
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
                 </div>
-                <div class="formrow">
-                    <span><img src="" alt=""></span>
-                    <textarea name="" id="" placeholder="Mensaje"></textarea>
+
+                <div class="form-row">
+                    <div class="privacy">
+                        <input type="checkbox">
+                        Acepto las condiciones de uso y la política de privacidad.
+                    </div>
                 </div>
-                <div class="form-control">
-                    <input type="checkbox">
-                </div>
-                <div class="form-control">
-                    <input type="submit" value="">
+
+                <div class="form-row">
+                    <input type="submit" value="Enviar">
                 </div>
             </div>
         </div>
         <div class="column">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3594.5339419913366!2d-100.23769678435328!3d25.719850616607378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8662eae71bf53af1%3A0x7dab57ce80cbffa5!2sF%26G%20GLOBAL%20EQUIPMENT!5e0!3m2!1ses-419!2smx!4v1579036476658!5m2!1ses-419!2smx"
-                width="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>
     </div>
 </div>
