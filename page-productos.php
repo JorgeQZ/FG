afd2da5eb95b47c389aac2008d24282168c49199
Byron-Guevara <byronguevara@akevia.mx>
2020-01-21 16:00:38 -0600

    Productos

---
+++ page-productos.php
@@ -0,0 +1,222 @@
+<?php
+/**
+ * Template Name: Productos
+ *
+ */
+?>
+<?php get_header();?>
+
+<div class="banner" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)">
+    <div class="banner-tit">
+        <?php the_title() ?>
+    </div>
+</div>
+
+<div class="home-wrapper contenedor-general-productos">
+    <div class="sliders-container">
+
+        <div class="slider-tabs">
+            <div class="item active">
+                <img src="<?php echo get_template_directory_uri().'/img/procentec.png'?>" alt="">
+            </div>
+            <div class="item">
+                <img src="<?php echo get_template_directory_uri().'/img/siemens.png'?>" alt="">
+
+            </div>
+            <div class="item">
+                <img src="<?php echo get_template_directory_uri().'/img/fg.png'?>" alt="">
+
+            </div>
+            <div class="item">
+                <img src="<?php echo get_template_directory_uri().'/img/omron.png'?>" alt="">
+            </div>
+            <div class="item">
+                <img src="<?php echo get_template_directory_uri().'/img/econ.png'?>" alt="">
+
+            </div>
+        </div>
+
+        <div class="slider-tabs-content">
+            <div class="tab active">
+                <div class="info">
+                    <p class="tit">
+                        procentec
+                    </p>
+                    <p class="desc">
+                        En F&G Global Equipment, somos distribuidores exclusivos de PROCENTEC para México. 
+                        Contamos con suministro de refacciones para tus redes PROFIBUS y PROFINET.
+                    </p>
+                    <p class="desc">
+                        Ofrecemos asesoría para la selección de equipos para redes 
+                        industriales en nuevos proyectos y ampliaciones.
+                    </p>
+                    <p class="desc">
+                        Brindamos cursos de capacitación técnica, cubriendo el uso del hardware y 
+                        software necesario para analiar y diagnosticar tus redes industriales.
+                    </p>
+                    <p class="desc">
+                        Da clic en las imágenes para conocer los productos, o bien a mano derecha en el 
+                        logo de PROCENTEC para ir directamente al sitio web.
+                    </p>
+                </div>
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="img-container"></div>
+                    <div class="text">
+                        <div class="title">
+                            ComBricks
+                        </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="ver-mas">ver más</div>
+            </div>
+            <div class="tab"></div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer();?>
